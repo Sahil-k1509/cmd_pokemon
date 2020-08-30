b30455544af09d63bc5d8d809f6e4fb7c9053eeb
Sahil-k1509 <sahilbairagi319@gmail.com>
2020-08-31 01:31:05 +0530

    Added pickle support to save and load game

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-import json
+import pickle
 
-from attacklist import *
-from attacks import Attack
 from player import PokemonTrainer
-from pokemon import Pokemon
 from pokeworld import pokemonWorld
+
+all_small_poke = ['pikachu', 'charmander', 'squirtle', 'bulbasaur']
+
+def save_game(player):
+    with open('pokemon_progess.pkl', 'wb') as output:
+        pickle.dump(player, output, pickle.HIGHEST_PROTOCOL)
+        
+def load_game():
+    with open('pokemon_progress.pkl', 'rb') as inputf:
+        player = pickle.load(inputf)
+    return player
+    
+
+def gameloop():
+    while True:
+        break
+    
+    
+    
+if __name__ == '__main__':    
+    gameloop()
